ef3a339cfc138efebaf7ccef41041b9c02709d16
pablohenrique <pablo@softbox.com.br>
2018-07-12 10:08:29 -0300

    fix(githooks): fixed verifyCommitMsg script

--- scripts/verifyCommitMsg.js
+++ scripts/verifyCommitMsg.js
@@ -12,6 +12,7 @@ if (!commitRE.test(msg)) {
     `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
     chalk.red(`  Proper commit message format is required for automated changelog generation. Examples:\n\n`) +
     `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+    `    ${chalk.green(`options: (feat|fix|docs|style|refactor|perf|test|workflow|ci|chore|types)`)}\n` +
     chalk.red(`  See .github/COMMIT_CONVENTION.md for more details.\n`)
     // chalk.red(`  You can also use ${chalk.cyan(`npm run commit`)} to interactively generate a commit message.\n`)
   )
