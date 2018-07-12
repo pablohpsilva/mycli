e4abcfad2e76756c4bd1b00063864a8ef6b6eb1d
pablohenrique <pablo@softbox.com.br>
2018-07-12 10:01:40 -0300

    second commit

--- scripts/verifyCommitMsg.js
+++ scripts/verifyCommitMsg.js
@@ -12,7 +12,7 @@ if (!commitRE.test(msg)) {
     `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
     chalk.red(`  Proper commit message format is required for automated changelog generation. Examples:\n\n`) +
     `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
-    chalk.red(`  See .github/COMMIT_CONVENTION.md for more details.\n`) +
+    chalk.red(`  See .github/COMMIT_CONVENTION.md for more details.\n`)
     // chalk.red(`  You can also use ${chalk.cyan(`npm run commit`)} to interactively generate a commit message.\n`)
   )
   process.exit(1)
